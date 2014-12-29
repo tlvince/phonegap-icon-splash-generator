95fe622b21cc45f87f2299b0a11c00349c4c4cab
Aleksey Meshkov <tomeshkov@gmail.com>
2014-12-29 15:45:31 +0300

    Update phonegap-icon-splash-generator.sh

--- phonegap-icon-splash-generator.sh
+++ phonegap-icon-splash-generator.sh
@@ -30,6 +30,9 @@ $convert "$1" -resize 50x50 "$3/res/icon/bada-wac/icon-50-type3.png"
 
 $convert "$1" -resize 80x80 "$3/res/icon/blackberry/icon-80.png"
 
+# iOS 8.0+
+# iPhone 6 Plus
+$convert "$1" -resize 180x180 "$3/res/icon/ios/icon-60@3x.png"
 # iOS 7.0+
 # iPhone / iPod Touch
 $convert "$1" -resize 60x60 "$3/res/icon/ios/icon-60.png"
@@ -80,13 +83,17 @@ $convert -resize 256x256 -extent 480x800 "$3/res/screen/bada-wac/screen-type5.pn
 
 $convert -resize 128x128 -extent 225x225 "$3/res/screen/blackberry/screen-225.png"
 
-$convert -resize 256x256 -extent 320x480 "$3/res/screen/ios/Default~iphone.png"
-$convert -resize 512x512 -extent 640x960 "$3/res/screen/ios/Default@2x~iphone.png"
+$convert -resize 256x256   -extent 320x480 "$3/res/screen/ios/Default~iphone.png"
+$convert -resize 512x512   -extent 640x960 "$3/res/screen/ios/Default@2x~iphone.png"
 $convert -resize 1024x1024 -extent 768x1024 "$3/res/screen/ios/Default-Portrait~ipad.png"
-$convert -resize 512x512 -extent 1536x2048 "$3/res/screen/ios/Default-Portrait@2x~ipad.png"
-$convert -resize 256x256 -extent 1024x768 "$3/res/screen/ios/Default-Landscape~ipad.png"
-$convert -resize 256x256 -extent 2048x1536 "$3/res/screen/ios/Default-Landscape@2x~ipad.png"
-$convert -resize 256x256 -extent 640x1136 "$3/res/screen/ios/Default-568h@2x~iphone.png"
+$convert -resize 1024x1024 -extent 1536x2048 "$3/res/screen/ios/Default-Portrait@2x~ipad.png"
+$convert -resize 1024x1024 -extent 1024x768 "$3/res/screen/ios/Default-Landscape~ipad.png"
+$convert -resize 1024x1024 -extent 2048x1536 "$3/res/screen/ios/Default-Landscape@2x~ipad.png"
+$convert -resize 512x512   -extent 640x1136 "$3/res/screen/ios/Default-568h@2x~iphone.png"
+
+$convert -resize 512x512 -extent 750x1334 "$3/res/screen/ios/Default-667h.png.png"
+$convert -resize 1024x1024 -extent 1242x2208 "$3/res/screen/ios/Default-736h.png.png"
+$convert -resize 1024x1024 -extent 2208x1242 "$3/res/screen/ios/Default-Landscape-736h.png.png"
 
 convert "$1" -resize 64x64 "$3/res/screen/webos/screen-64.png"
 
